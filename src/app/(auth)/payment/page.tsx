6238189251d60f9d1b90fdbeaf1a6f389f6f7e52
Badi1298 <serbandavid83@yahoo.com>
2024-07-06 15:53:52 +0300

    384

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useTransition } from 'react';
+
 import { createCheckoutSession } from '@/actions/actions';
 
 import H1 from '@/components/h1';
@@ -7,21 +9,27 @@ import H1 from '@/components/h1';
 import { Button } from '@/components/ui/button';
 
 export default function Page({ searchParams }) {
+	const [isPending, startTransition] = useTransition();
+
 	return (
 		<main className="flex flex-col items-center space-y-10">
 			<H1>PetSoft access requires payment</H1>
 
 			{!searchParams.success && (
 				<Button
-					onClick={async () => {
-						await createCheckoutSession();
+					disabled={isPending}
+					onClick={() => {
+						startTransition(async () => {
+							await createCheckoutSession();
+						});
 					}}
 				>
-					Buy lifetime access for $299
+					{isPending ? 'Loading...' : 'Buy lifetime access for $299'}
 				</Button>
 			)}
 
 			{searchParams.success && <p className="text-sm text-green-700">Payment successful! You now have lifetime access to PetSoft.</p>}
+			{searchParams.canceled && <p className="text-sm text-red-700">Payment cancelled. You can try again.</p>}
 		</main>
 	);
 }
